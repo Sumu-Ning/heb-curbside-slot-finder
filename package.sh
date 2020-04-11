11ca8f738050de138b89c343d25783e39f888529
Pingsan Song <pingsans@amazon.com>
2020-04-11 16:25:03 -0500

    Update input height, remove extra wrapping in zip

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm heb-curbside-slot-finder.zip;
-zip -r heb-curbside-slot-finder.zip heb-curbside-slot-finder;
+zip -r heb-curbside-slot-finder.zip heb-curbside-slot-finder/*;
